c6e02d3ae8df617d2081a5fadf4cc0928988faf1
Katia Ghezali <ghezalikatia@hotmail.com>
2023-08-27 16:24:00 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+
+main.remove()
+
+const newHeader = document.createElement("h1");
+newHeader.textContent = "KATIA is the champion";
+newHeader.setAttribute("id", "victory");
+
+
